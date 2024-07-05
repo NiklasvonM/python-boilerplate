8cae10cb9c8fe33136fc1585b449ef3d987b38a9
NiklasvonM <niklas.von.moers2@t-online.de>
2024-07-05 18:59:45 +0200

    fix type annotation

--- scripts/use_custom_logger.py
+++ scripts/use_custom_logger.py
@@ -3,7 +3,14 @@ import json
 import logging.config
 import logging.handlers
 import pathlib
-from typing import Any
+from logging.handlers import QueueListener
+from typing import Any, Protocol, runtime_checkable
+
+
+@runtime_checkable
+class HasQueueListener(Protocol):
+    listener: QueueListener
+
 
 logger = logging.getLogger("my_app")  # __name__ is a common choice
 
@@ -15,7 +22,7 @@ def setup_logging() -> None:
 
     logging.config.dictConfig(config)
     queue_handler = logging.getHandlerByName("queue_handler")
-    if queue_handler is not None:
+    if queue_handler is not None and isinstance(queue_handler, HasQueueListener):
         queue_handler.listener.start()
         atexit.register(queue_handler.listener.stop)
 
